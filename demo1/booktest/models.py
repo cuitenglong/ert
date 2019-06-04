b4a1c44b1be968ccc764f412219c728206c4cf27
ctl <954471818@qq.com>
2019-06-04 21:53:49 +0800

    day6/4

---
+++ demo1/booktest/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class BookInfo(models.Model):
+    """图书类"""
+    title = models.CharField(max_length=20)
+    pub_date = models.DateTimeField()
+
+
+class HeroInfo(models.Model):
+    """英雄类"""
+    name = models.CharField(max_length=20)
+    gender = models.BooleanField(default=True)
+    content = models.CharField(max_length=100)
+    # book作为外键关联到表
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
